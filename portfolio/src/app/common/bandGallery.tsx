41d3786ed2dc02bcfba4899874e4ed5b850b4864
wanderingduc <dkdanid@outlook.com>
2025-05-02 13:28:30 +0200

    Add section title

--- portfolio/src/app/common/bandGallery.tsx
+++ portfolio/src/app/common/bandGallery.tsx
@@ -9,19 +9,21 @@ interface Item {
 function BandGallery() {
 
     //For future integration of fetch
-    const data: Array<Item> = [{'img': '', 'name': 'name1', 'desc': 'desc of name1'}, {'img': '', 'name': 'name2', 'desc': 'desc of name2'}, {'img': '', 'name': 'name3', 'desc': 'desc of name3'}, {'img': '', 'name': 'name4', 'desc': 'desc of name4'}]
+    const data: Array<Item> = [{'img': '/featureplaceholder.png', 'name': 'name1', 'desc': 'desc of name1'}, {'img': '/featureplaceholder.png', 'name': 'name2', 'desc': 'desc of name2'}, {'img': '/featureplaceholder.png', 'name': 'name3', 'desc': 'desc of name3'}, {'img': '/featureplaceholder.png', 'name': 'name4', 'desc': 'desc of name4'}]
 
     return (
         <div className={styles.container}>
+            <h2 className={styles.header}>Projects</h2>
             {data.map((item) => {
                 return (
-                <div className={styles.itemContainer} >
-                    <div className={styles.itemTextContainer} >
-                        <h4 className={styles.itemName} >{item.name}</h4>
-                        <p className={styles.itemDesc} >{item.desc}</p>
+                    <div key={item.name} className={styles.itemContainer} >
+                        <div className={styles.itemTextContainer} >
+                            <h4 className={styles.itemName} >{item.name}</h4>
+                            <p className={styles.itemDesc} >{item.desc}</p>
+                        </div>
+                        <img className={styles.itemImage} src={item.img} alt="Picture describing the name" />
                     </div>
-                    <img className={styles.itemImage} src={item.img} alt="Picture describing the name" />
-                </div>)
+                )
             })}
         </div>
     )
