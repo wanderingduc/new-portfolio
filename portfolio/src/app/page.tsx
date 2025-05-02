019a631d740a43f694f5eb7dbe9007949f43ec57
wanderingduc <dkdanid@outlook.com>
2025-05-02 13:37:10 +0200

    Wrap elements in fadeInWrapper

--- portfolio/src/app/page.tsx
+++ portfolio/src/app/page.tsx
@@ -3,13 +3,19 @@ import styles from "./page.module.css";
 import Nav from "./common/nav";
 import Banner from "./common/banner";
 import BandGallery from "./common/bandGallery";
+import FadeInOnScrollWrapper from "./common/fadeInWrapper";
+
 
 export default function Home() {
   return (
     <>
       <Nav />
+      <FadeInOnScrollWrapper>
       <Banner />
+      </FadeInOnScrollWrapper>
+      <FadeInOnScrollWrapper>
       <BandGallery />
+      </FadeInOnScrollWrapper>
     </>
   );
 }
