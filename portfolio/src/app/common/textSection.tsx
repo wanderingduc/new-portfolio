048c23b89ce0b43cf7c3ab473cf2c3ba06734e91
wanderingduc <dkdanid@outlook.com>
2025-05-02 15:11:18 +0200

    Make textSection element

---
+++ portfolio/src/app/common/textSection.tsx
@@ -0,0 +1,56 @@
+import styles from './textSection.module.css';
+
+interface buttonProp {
+    download: boolean;
+    text: string | null;
+    href: string
+}
+
+interface textProp {
+    title: string | null;
+    text: string | null;
+    button: buttonProp;
+}
+
+function TextSection(props: textProp) {
+
+
+    if(props.button){
+        if(props.button.download){
+            return (
+                <div className={styles.container}>
+                    <h2 className={styles.header}>{props.title}</h2>
+                    <p className={styles.text}>{props.text}</p>
+                    <a className={styles.buttonContainer} href={props.button.href} download={true}>
+                        <button className={styles.downloadButton}>
+                                <p className={styles.buttonText}>{props.button.text}</p>
+                                <span className={styles.materialSymbolsOutlined}>download</span>
+                        </button>
+                    </a>
+                </div>
+            )
+        }
+        return (
+            <div className={styles.container}>
+                <h2 className={styles.header}>{props.title}</h2>
+                <p className={styles.text}>{props.text}</p>
+                <a className={styles.buttonContainer} href={props.button.href} >
+                    <button className={styles.button}>
+                            <p className={styles.buttonText}>{props.button.text}</p>
+                    </button>
+                </a>
+            </div>
+        )
+    }
+
+
+    return(
+        <div className={styles.container}>
+            <h2 className={styles.header}>{props.title}</h2>
+            <p className={styles.text}>{props.text}</p>
+        </div>
+    )
+
+}
+
+export default TextSection;
