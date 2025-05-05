6621e7f8318a592e89b8be3ecf0e583c63a07c6d
wanderingduc <dkdanid@outlook.com>
2025-05-05 13:40:28 +0200

    Add repo link

--- portfolio/src/app/common/bandGallery.tsx
+++ portfolio/src/app/common/bandGallery.tsx
@@ -1,16 +1,18 @@
+import Link from "next/link";
 import styles from "./bandGallery.module.css";
 
 interface Item {
     img: string;
     name: string;
     desc: string;
+    url: string | null;
 }
 
 function BandGallery() {
 
     //For future integration of fetch
-    const data: Array<Item> = [{'img': '/featureplaceholder.png', 'name': 'name1', 'desc': 'desc of name1'}, {'img': '/featureplaceholder.png', 'name': 'name2', 'desc': 'desc of name2'}, {'img': '/featureplaceholder.png', 'name': 'name3', 'desc': 'desc of name3'}, {'img': '/featureplaceholder.png', 'name': 'name4', 'desc': 'desc of name4'}]
-
+    const data: Array<Item> = [{'img': '/featureplaceholder.png', 'name': 'Jet Engine Controll and Monitoring', 'desc': 'Made a controll and monitoring system for a KingTech K180 G5 jet engine', 'url': null}, {'img': '/featureplaceholder.png', 'name': 'Stock Analysis Program', 'desc': 'Made an analysis program for analyzing stocks and giving buy/sell recomendations for a client', 'url': 'https://github.com/wanderingduc/tt'}, {'img': '/featureplaceholder.png', 'name': 'Honbako', 'desc': 'An app for cataloging and sharing books between peers', 'url': 'https://github.com/wanderingduc/yomiya-mobile'}, {'img': '/featureplaceholder.png', 'name': 'Custom Mechanical Keyboards', 'desc': 'Designed and prototyped multiple mechanical keyboards', 'url': null}]
+    https://github.com/wanderingduc/yomiya-mobile
     return (
         <div className={styles.container}>
             <h2 className={styles.header}>Projects</h2>
@@ -20,6 +22,7 @@ function BandGallery() {
                         <div className={styles.itemTextContainer} >
                             <h4 className={styles.itemName} >{item.name}</h4>
                             <p className={styles.itemDesc} >{item.desc}</p>
+                            {item.url ? <a className={styles.itemLink} href={item.url}>Github</a> : null}
                         </div>
                         <img className={styles.itemImage} src={item.img} alt="Picture describing the name" />
                     </div>
